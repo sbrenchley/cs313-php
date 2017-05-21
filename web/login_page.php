d8e76826fdb9f34ebc240d61bbc3394c90f53924
Suzanne Brenchley <kem14002@byui.edu>
2017-05-20 20:28:27 -0600

    modified

--- web/login_page.php
+++ web/login_page.php
@@ -5,7 +5,7 @@
 </head>
 <body>
   <?php
-      echo 'Working';
+      echo 'Working 3';
     if (isset($_POST['submit']))
     {
       include("config.php");
@@ -51,7 +51,7 @@
     }
   ?>
   <h1>Saved Pages</h1>
-  <form action="login_page.php">
+  <form action="login_page.php" method="post">
     <div class="container">
       <label><b>Username</b></label>
       <input type="text" placeholder="Enter Username" name="username" required>
