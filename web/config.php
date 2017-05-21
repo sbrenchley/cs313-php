8c72ba499cf01dfa2afddfdd4c74aa7a23331456
Suzanne Brenchley <kem14002@byui.edu>
2017-05-20 20:52:24 -0600

    modified

--- web/config.php
+++ web/config.php
@@ -16,8 +16,9 @@
    try {
      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
    }
-   catch (PDOException $ex) {
-     print "<p>error: $ex->getMessage() </p>\n\n";
+   catch (Exception $ex) {
+     echo $ex->getMessage();
+//     print "<p>error: $ex->getMessage() </p>\n\n";
      die();
    }
 ?>
