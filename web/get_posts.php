37867b198e364ec23da321c093cf6eda1da9c9d2
Suzanne Brenchley <kem14002@byui.edu>
2017-05-27 13:14:48 -0600

    updated link

--- web/get_posts.php
+++ web/get_posts.php
@@ -1,5 +1,5 @@
 <?php
   session_start();
   include 'header.php';
-  header('Location: https://www.reddit.com/api/v1/authorize?client_id=CLIENT_ID&response_type=TYPE&state=RANDOM_STRING&redirect_uri=URI&duration=DURATION&scope=SCOPE_STRING')
+  header('Location: https://www.reddit.com/api/v1/authorize?client_id=brenchley_saved_posts&response_type=code&state=suzanne&redirect_uri=https://ancient-wave-30284.herokuapp.com/saved_posts.php&duration=temporary&scope=history')
 ?>
