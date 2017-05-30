437b92ebec7dd270447ec6effebfef117c816f1d
Suzanne Brenchley <kem14002@byui.edu>
2017-05-29 21:19:57 -0600

    fixed date

--- web/get_posts.php
+++ web/get_posts.php
@@ -124,8 +124,8 @@
           echo $post->data->title;
       //    $stmt = $db->prepare('INSERT INTO saved_posts (title, saved_date, subreddit, votes, link, user_id) VALUES (:title, :saved_date, :subreddit, :score, :permalink, :id)');
 
-          $stmt = $db->prepare('INSERT INTO saved_posts (title, saved_date, subreddit, votes, link, user_id) VALUES ("' . $post->data->title . '", ' . $post->data->created_utc . ', "' . $post->data->subreddit . '", ' . $post->data->score . ', "' . $post->data->permalink . '", ' . $_SESSION['login_id'] . ')');
-          echo 'INSERT INTO saved_posts (title, saved_date, subreddit, votes, link, user_id) VALUES ("' . $post->data->title . '", ' . $post->data->created_utc . ', "' . $post->data->subreddit . '", ' . $post->data->score . ', "' . $post->data->permalink . '", ' . $_SESSION['login_id'] . ')';
+          $stmt = $db->prepare("INSERT INTO saved_posts (title, saved_date, subreddit, votes, link, user_id) VALUES ('" . $post->data->title . "', '05/30/2017', '" . $post->data->subreddit . "', '" . $post->data->score . "', '" . $post->data->permalink . "', '" . $_SESSION['login_id'] . "')");
+          echo "INSERT INTO saved_posts (title, saved_date, subreddit, votes, link, user_id) VALUES ('" . $post->data->title . "', '05/30/2017', '" . $post->data->subreddit . "', '" . $post->data->score . "', '" . $post->data->permalink . "', '" . $_SESSION['login_id'] . "')");
 ;
 /*
           $stmt->bindValue(':title', $post->data->title, PDO::PARAM_STR);
