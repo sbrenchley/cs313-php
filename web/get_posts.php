902d72efc429fc2fe128d7a3ceb482701ed269d2
Suzanne Brenchley <kem14002@byui.edu>
2017-05-29 19:11:13 -0600

    fetching posts

--- web/get_posts.php
+++ web/get_posts.php
@@ -67,7 +67,7 @@
     }
   }
 
-/*  if ($_SESSION['reddit_state'] === "have_token") {
+  if ($_SESSION['reddit_state'] === "have_token") {
     //TODO hard-coding this for now. need to request it via reddit api
     $_SESSION['reddit_user'] = 'sbrenchley';
 
@@ -75,34 +75,38 @@
 
     //request the user's saved posts
     $url = "https://oauth.reddit.com/api/v1/user/" . $_SESSION['reddit_user'] . "/saved";
-    $data = array('grant_type' => 'authorization_code', 'code' => $_GET['code'], 'redirect_uri' => 'https://ancient-wave-30284.herokuapp.com/get_posts.php');
+    // $data = array('grant_type' => 'authorization_code', 'code' => $_GET['code'], 'redirect_uri' => 'https://ancient-wave-30284.herokuapp.com/get_posts.php');
 
     // use key 'http' even if you send the request to https://...
     $options = array(
       'http' => array(
-        'header'  => ["Content-type: application/json\r\n",
-                      "Authorization: bearer " . $_SESSION['reddit_token'] . "\r\n"],
-        'method'  => 'POST',
-        'content' => http_build_query($data)
+        'header'  => "Authorization: bearer " . $_SESSION['reddit_token'] . "\r\n" .
+                     "Accept: application/json\r\n",
+        'method'  => 'GET'
       )
     );
+
+    debug("about to make saved posts call");
+
     $context  = stream_context_create($options);
     $result = file_get_contents($url, false, $context);
+
     if ($result === FALSE) {
+      debug("posts call: broke");
       //something went wrong
       $_SESSION['reddit_state'] = "initial";
       echo "<script language='javascript' type='text/javascript'> location.href='saved_posts.php' </script>";
     }
     //everything went right
     else {
+      debug("posts call debug so chill");
       //put data into php array
-      $result_data = json_decode($result);
-      $_SESSION['reddit_token'] = $result_data->access_token;
-      $_SESSION['reddit_state'] = "have_token";
+      // $result_data = json_decode($result);
+      // $_SESSION['reddit_token'] = $result_data->access_token;
+      // $_SESSION['reddit_state'] = "have_token";
     }
     var_dump($result);
   }
-  */
 ?>
 
 
